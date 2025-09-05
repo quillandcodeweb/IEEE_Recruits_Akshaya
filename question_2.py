07577a286a5996e56fa084e5117f7b3c0ef6803f
quillandcodeweb <akshaya.sutrave@gmail.com>
2025-09-06 00:01:48 +0530

    question_2.py

--- question_2.py
+++ question_2.py
@@ -4,14 +4,14 @@ text = input("Enter a paragraph of max 100 words: ")
 words = text.split() #split the paragraph into words
 palindrome=[]
 if len(words)>100:
-    print ("Error. Input exceeds 100 words")
+    print ("Error. Input exceeds 100 words") #error message if the paragraph exceeds 100 words
 else:
     for l in words:
-        if l==l[::-1]:
+        if l==l[::-1]: #checking if every word is a palindrome
             print(l)
-            palindrome.append(l)
+            palindrome.append(l) #adding to a list of palindromes
 
-if len(palindrome)==0:
+if len(palindrome)==0: #in case of no palindromes
     print(0)
 
 
